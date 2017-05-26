1e7fabee77d8de18d13ce91eed49522bf5f81f09
Cindy Zou <xzou@email.wm.edu>
2017-05-25 21:22:01 -0400

    Create quiz

--- src/app/pd-test-question/pd-test-question.component.ts
+++ src/app/pd-test-question/pd-test-question.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
 
+import { Player } from '../players/player';
+
 @Component({
   selector: 'pd-test-question',
   templateUrl: './pd-test-question.component.html',
   styleUrls: ['./pd-test-question.component.css']
 })
+
 export class PDTestQuestionComponent {
+  public player: Player;
+   
+  choiceAText: string = `Cooperate means I don't pay anything and 
+  can still collect points. Defect means I pay 20 of my own
+  points to give the other player 40 points`;
+  choiceBText: string = `Cooperate means I pay 20 of my own points to 
+  give the other player 40 points. Defect means I don't pay 
+  anything and can still collect points.`;
+  feedbackCor: string = 'Correct! You may enter the game.';
+  feedbackIncor: string = 'Incorrect. This session will now be terminated. Thank you!';
+  
+  selectedOption: string;
+  feedback: string;
+  is_correct: boolean;
+  is_answered: boolean = false;
+  is_submitted: boolean = false;
 
-  constructor() { }
+  isAnswered() {
+    if (this.selectedOption === '1' || this.selectedOption === '2') {
+      this.is_answered = true;
+      return this.is_answered;
+    }
+    else 
+      console.log('Question not answered');
+  }
 
+  checkAnswer() {
+    console.log('Initial: ', this.is_submitted);
+    if (this.selectedOption === '1') {
+      this.feedback = this.feedbackIncor;
+      this.is_correct = false;
+      this.is_submitted = true;
+      console.log('After: ', this.is_submitted);
+    }
+    else if (this.selectedOption === '2') {
+      this.feedback = this.feedbackCor;
+      this.is_correct = true;
+      this.is_submitted = true;
+      console.log('After 2: ', this.is_submitted);
+    }
+    else 
+      console.log('Invalid value of quiz answer');
+  } 
+
+  isSubmitted() {
+    return this.is_submitted;
+  }
 }
+
