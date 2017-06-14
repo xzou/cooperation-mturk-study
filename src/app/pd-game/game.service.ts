4bca46523cfac39a60e9aa71b600f0ed599dc8d3
Cindy Zou <xzou@email.wm.edu>
2017-06-14 17:02:56 -0400

    Change to condition 1 .65

--- src/app/pd-game/game.service.ts
+++ src/app/pd-game/game.service.ts
@@ -7,9 +7,9 @@ export class GameService {
 
   maxRounds: number = 80; 
   firstSlider: number = 10;
-  condition: number = 2;
+  condition: number = 1;
 
-  pCoop1: number = .35;
+  pCoop1: number = .65;
   pCoop2: number = 1-this.pCoop1;
   pCoop: number = this.pCoop1;
 
